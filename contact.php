9402be5135b553caaba903aa241b2a9af8dc7084
Conner Guidry <guidryconner@gmail.com>
2019-04-10 11:22:52 -0500

    New key

--- contact.php
+++ contact.php
@@ -23,7 +23,7 @@ $okMessage = 'Contact form successfully submitted. Thank you, I will get back to
 $errorMessage = 'There was an error while submitting the form. Please try again later';
 
 // ReCaptch Secret
-$recaptchaSecret = '6Lcy-ZwUAAAAAFg07OaU2iUflozFBdOnxVWZyped';
+$recaptchaSecret = '6Ldtcp0UAAAAABRnUyez5ZdapB86NPHcoAkZSy_8';
 
 // let's do the sending
 
